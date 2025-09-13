51859f7c3a61ebdb958f49452baa03603144d0f5
Muktary-Hub <mukhtarabdulwaheed7@gmail.com>
2025-09-13 02:53:35 +0100

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -1,18 +1,19 @@
-export function sendMessageWithDelay(msg, text) {
-    const delay = Math.floor(Math.random() * 1000) + 1500;
+const getRandomReply = (replies) => {
+    return replies[Math.floor(Math.random() * replies.length)];
+};
+
+const sendMessageWithDelay = (msg, text) => {
+    const delay = Math.floor(Math.random() * 800) + 1200; // Delay between 1.2 and 2 seconds
     return new Promise(resolve => setTimeout(() => msg.reply(text).then(resolve), delay));
-}
+};
 
-export function getRandomReply(replies) {
-    const randomIndex = Math.floor(Math.random() * replies.length);
-    return replies[randomIndex];
-}
-
-export function isSubscriptionActive(user, adminNumbers) {
+const isSubscriptionActive = (user, ADMIN_NUMBERS) => {
     if (!user) return false;
-    if (adminNumbers.includes(user.userId)) return true;
+    if (ADMIN_NUMBERS.includes(user.userId)) return true;
     if (!user.isPaid || !user.subscriptionExpiryDate) {
         return false;
     }
     return new Date() < new Date(user.subscriptionExpiryDate);
-}
+};
+
+module.exports = { getRandomReply, sendMessageWithDelay, isSubscriptionActive };
