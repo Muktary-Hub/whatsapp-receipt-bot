db2919f25edf5560125710de44c3677c26731270
Muktary-Hub <mukhtarabdulwaheed7@gmail.com>
2025-09-15 15:51:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function parseInputList(text) {
 
 // --- BUSINESS MODEL ---
 const SUBSCRIPTION_FEE = 2000;
-const FREE_TRIAL_LIMIT = 3;
+const FREE_TRIAL_LIMIT = 2;
 const FREE_EDIT_LIMIT = 2;
 
 // --- Configuration ---
